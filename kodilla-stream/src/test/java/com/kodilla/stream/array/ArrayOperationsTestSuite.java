d0d2ad56eba99d9b6c4f4de4952ef619613a81bc
Albert Kowal <kowal.albert.90@gmail.com>
2022-10-27 01:36:37 +0200

    7.5

---
+++ kodilla-stream/src/test/java/com/kodilla/stream/array/ArrayOperationsTestSuite.java
@@ -0,0 +1,27 @@
+package com.kodilla.stream.array;
+
+import org.junit.jupiter.api.Test;
+
+import javax.swing.text.html.Option;
+import java.util.OptionalDouble;
+import java.util.stream.IntStream;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ArrayOperationsTestSuite  {
+
+    @Test
+    void testGetAverage() {
+        //Given
+        int[] numbers = new int[20];
+        for (int i = 0; i < numbers.length; i++) {
+            numbers[i] = i + 10;
+        }
+
+        //When
+        OptionalDouble testingAverage = ArrayOperations.getAverage(numbers);
+
+        //Then
+        assertEquals(OptionalDouble.of(19.5), testingAverage);
+    }
+}
