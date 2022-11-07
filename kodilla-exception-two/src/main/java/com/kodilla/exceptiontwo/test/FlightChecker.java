3763569f03b602fecf9f36636ff60faac9fe6563
Albert Kowal <kowal.albert.90@gmail.com>
2022-11-07 23:06:16 +0100

    8.4 poprawa

--- kodilla-exception-two/src/main/java/com/kodilla/exceptiontwo/test/FlightChecker.java
+++ kodilla-exception-two/src/main/java/com/kodilla/exceptiontwo/test/FlightChecker.java
@@ -6,9 +6,6 @@ import java.util.Map;
 public class FlightChecker {
 
     public static boolean findFlight(Flight flight) throws RouteNotFoundException {
-        boolean errorDeparture = true;
-        boolean errorArrival = true;
-        boolean landing = false;
 
         Map<String, Boolean> airports = new HashMap<>();
         airports.put("Chicago", false);
@@ -16,6 +13,10 @@ public class FlightChecker {
         airports.put("Miami", false);
         airports.put("New York", true);
         airports.put("Seattle", true);
+
+        /*
+        boolean errorDeparture = true;
+        boolean errorArrival = true;
 
         for (Map.Entry<String, Boolean> entry : airports.entrySet()) {
             if (flight.getArrivalAirport().equals(entry.getKey())) {
@@ -28,6 +29,23 @@ public class FlightChecker {
         if (!errorArrival) {
             if (!errorDeparture) {
                 if(landing) {
+                    System.out.println("Flight from " + flight.getDepartureAirport() + " to " + flight.getArrivalAirport() + " checked and verified.");
+                    return true;
+                } else {
+                    System.out.println("Flight from " + flight.getDepartureAirport() + " to " + flight.getArrivalAirport() + " checked but arrival at the chosen airport is unavailable.");
+                    return false;
+                }
+            } else {
+                throw new RouteNotFoundException("Departure airport incorrect.");
+            }
+        } else {
+            throw new RouteNotFoundException("Arrival airport incorrect.");
+        }
+        */
+
+        if (airports.containsKey(flight.getArrivalAirport())) {
+            if (airports.containsKey(flight.getDepartureAirport())) {
+                if (airports.get(flight.getArrivalAirport())) {
                     System.out.println("Flight from " + flight.getDepartureAirport() + " to " + flight.getArrivalAirport() + " checked and verified.");
                     return true;
                 } else {
